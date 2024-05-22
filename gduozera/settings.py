8368a8cfd55055309bf8032a776111ba879e3f9a
Eremeev Aleksandr <aleksandreremeev75@gmail.com>
2024-05-22 14:28:34 +0300

    beta 2.0

--- gduozera/settings.py
+++ gduozera/settings.py
@@ -97,7 +97,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'lending/static/'
 STATIC_ROOT = 'static/'
 
 MEDIA_URL = 'media/'
